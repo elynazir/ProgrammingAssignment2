219562bee265d38a0452e183d57dacc38009f0e1
Ely Nazir <elynazir@users.noreply.github.com>
2015-07-27 03:05:03 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The first function, makeVector creates a special "vector", which is really a list containing a function to
+## 1. set the value of the vector
+## 2. get the value of the vector
+## 3. set the value of the mean
+## 4. get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  # initialize the inverse matrix value
+  inv <- NULL
+  
+  # set the value of the matrix
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  # get the value of the matrix
+  get <- function() x
+  
+  # set the value of the inverse
+  set_inverse <- function(inv_input) inv <<- inv_input
+  # get the value of the inverse
+  get_inverse <- function() inv
+  
+  # return a list of all the above functions
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
+  
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the mean of the special "vector" 
+## created with the above function. However, it first checks to see 
+## if the mean has already been calculated. If so, it gets the mean 
+## from the cache and skips the computation. Otherwise, it calculates 
+## the mean of the data and sets the value of the mean in the cache 
+## via the setmean function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  # check if the inverse is already cached,
+  # if so, we get the inverse from the cache directly
+  inv <- x$get_inverse()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  # else, we first get the matrix
+    data <- x$get()
+  
+  # and calculate the inverse
+  inv <- solve(data, ...)
+  
+  # next, cache the inverse of the matrix
+  x$set_inverse(inv)
+  # and finally, return the result
+  inv
 }
